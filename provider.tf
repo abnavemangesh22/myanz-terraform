764fc3d517a95a9e5cae522adaccc6cebed099e4
abnavemangesh22 <69311384+abnavemangesh22@users.noreply.github.com>
2023-09-25 21:34:37 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,8 +1,8 @@
 provider "aws" {
   #access_key = "AKIAUIHY56BJ7LO4LKHH"
   #secret_key = "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"
-  region     = "ap-south-1"
-  shared_credentials_files = ["/root/.aws/credentials"]
-  shared_config_files      = ["/root/.aws/conf"]
-   #profile = "default"
+  #region     = "ap-south-1"
+  #shared_credentials_files = ["/root/.aws/credentials"]
+  #shared_config_files      = ["/root/.aws/conf"]
+  profile = "default"
 }
